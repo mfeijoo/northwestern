f97f70cdb7aed063ea58fdbe3a66c406e2c44fa9
Marcos Feijoo <mfeijoo@bluephysicsmed.com>
2024-02-01 09:50:54 -0500

    Update ultra_fast.py

--- pages/ultra_fast.py
+++ pages/ultra_fast.py
@@ -71,8 +71,8 @@ dfz['chunk'] = dfz.number // int(300000/750)
 dfg = dfz.groupby('chunk').agg({'time':np.median, 'ch0z':np.sum, 'ch1z':np.sum})
 
 #Calculate local maximuns
-ordernow = st.number_input('select order to find max of profiles', min_value = 1, max_value = 10, value = 8)
-maximuns = scipy.argrelextrema(dfg.ch0z.to_numpy(), np.greater, order = ordernow)
+#ordernow = st.number_input('select order to find max of profiles', min_value = 1, max_value = 10, value = 8)
+maximuns = scipy.argrelextrema(dfg.ch0z.to_numpy(), np.greater, order = 8)
 maximuns_times = dfg.iloc[maximuns[0][2:-2], 0].to_list()
 
 dfg0 = dfg.loc[:,['time', 'ch0z']]
